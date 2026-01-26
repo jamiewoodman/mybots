17ba79c2b6fcd086af43bcc5182ca547801d9b35
jamiewoodman <jewoodma@uvm.edu>
2026-01-26 14:32:44 -0500

    Assignment 2 completed (Links)

--- generate.py
+++ generate.py
@@ -1,1 +1,18 @@
-pass
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("boxes.sdf")
+for k in range(5):
+    for j in range(5):
+        length = 1
+        width = 1
+        height = 1
+        x = k
+        y = j
+        z = .5
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box"+str(i), pos=[x,y,z] , size=[length,width,height])
+            length = length * .9
+            width = width * .9
+            height = height * .9
+            z += (height / 2) + (height / 2 / .9)
+pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,6 +1,12 @@
 import pybullet as p
 import time
+import pybullet_data
+
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8,physicsClient)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for i in range(0, 1000):
     p.stepSimulation()
     print(i)
